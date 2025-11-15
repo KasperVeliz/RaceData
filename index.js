ba1f522157ff9c55d2ebac84cd63e4f0e111eeaf
Kasper <kasper.veliz@gmail.com>
2025-11-15 15:53:34 -0500

    finished driver api

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ app.use(
 
 app.use(function(request, response, next){
     response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
-    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers");
+    response.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+    response.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers");
     next();
 })
 
