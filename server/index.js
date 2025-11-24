c20e53ce33b60ca8706d37000bc14c3aec442ccc
Kasper <kasper.veliz@gmail.com>
2025-11-23 21:46:20 -0500

    hosting with Railway

--- server/index.js
+++ server/index.js
@@ -7,14 +7,14 @@ const port = 3001
 
 const db = require('./queries')
 
-app.use(express.static(path.join(__dirname, '../frontend/dist')))
+app.use(express.static(path.resolve(__dirname, '../frontend/dist')))
 
 app.use(express.json())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true,}))
 
 app.get('/', (request, response) => {
-	response.sendFile(express.static(path.join(__dirname, '../frontend/dist', 'index.html')))
+	response.sendFile(express.static(path.resolve(__dirname, '../frontend/dist', 'index.html')))
 })
 app.get('/drivers', db.getAllDrivers)
 app.get('/drivers/:id/laps', db.getDriverLapsById)
